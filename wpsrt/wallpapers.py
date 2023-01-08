987a99c8a15a4062fdd3f5a527085bf5e95ab59d
jpk <jpk@goatpr0n.de>
2023-01-08 09:58:10 +0100

    Handle non image files safely

--- wpsrt/wallpapers.py
+++ wpsrt/wallpapers.py
@@ -2,15 +2,18 @@ import os
 from pathlib import Path
 from typing import Iterable, Tuple
 
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 
 
 def scan_directory(directory: Path) -> Iterable[Tuple[Path, Tuple[int, int]]]:
     for root, _, filenames in os.walk(directory):
         for filename in [Path(os.path.join(root, fname)) for fname in filenames]:
             if filename.is_file():
-                image = Image.open(filename)
-                yield (filename, image.size)
+                try:
+                    image = Image.open(filename)
+                    yield (filename, image.size)
+                except UnidentifiedImageError:
+                    continue
 
 
 def move_wallpaper(wallpaper: Path, target: Path) -> Path:
