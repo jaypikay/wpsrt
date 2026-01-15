d976f3c81f982eb6a770674ec12fa55d009523c8
jpk <jpk@goatpr0n.de>
2026-01-15 20:59:10 +0100

    Add insepctor script with grepable output for NSFW content

---
+++ src/wpsrt/inspector.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+
+import click
+from nudenet import NudeDetector
+
+from wpsrt.wallpapers import scan_directory
+
+
+def grep_beautifier(classification: dict) -> str:
+    output = []
+    for data in classification:
+        output.append(f"{data['class']}={data['score']}")
+    if output:
+        return ":".join(output)
+    else:
+        return "SFW"
+
+
+@click.command()
+@click.option(
+    "-n",
+    "--nsfw-model",
+    type=click.Path(exists=True, file_okay=True, dir_okay=False),
+    default=None,
+)
+@click.option(
+    "-j",
+    "--jobs",
+    type=int,
+    default=4,
+)
+@click.argument(
+    "target",
+    type=click.Path(exists=True, file_okay=True, dir_okay=True),
+    default=Path("~/Pictures/wallpapers").expanduser(),
+)
+def nsfw_inspect(nsfw_model: Path, jobs: int, target: Path) -> None:
+    if nsfw_model and Path(nsfw_model).exists():
+        detector = NudeDetector(model_path=nsfw_model)
+    else:
+        detector = NudeDetector()
+
+    target = Path(target)
+    if target.is_dir():
+        found_files = [f.as_posix() for f in scan_directory(target)]
+        if len(found_files) > 100:
+            msg = " this may take a while, please wait"
+        else:
+            msg = ""
+        click.echo(f"Processing {len(found_files)}{msg}...", err=True)
+        classifications = detector.detect_batch(found_files, batch_size=jobs)
+        for fname, classification in zip(found_files, classifications):
+            click.echo(f"{fname}:{grep_beautifier(classification)}")
+    else:
+        classification = detector.detect(target.as_posix())
+        click.echo(f"{target}:{grep_beautifier(classification)}")
